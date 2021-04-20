eda1679cecaf4d029cd8c6690a9218fc91fc554b
Selma Haracic <selma.haracic@systech.ba>
2021-04-20 09:12:27 +0200

    tut 27

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,38 +1,62 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { forbiddenNameValidator } from './shared/user-name.validator'
+import { passwordValidator } from './shared/password.validator'
+import { RegistrationService } from './registration.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor( private fb: FormBuilder){}
+  registrationForm: FormGroup;
+
+  constructor( private fb: FormBuilder,
+               private reistrationService: RegistrationService ){}
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  get alternateEmails() {
+    return this.registrationForm.get('alternateEmails') as FormArray;
+  }
+
+  addAlternateEmail() {
+    this.alternateEmails.push(this.fb.control(''));
+  }
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required,Validators.minLength(3), forbiddenNameValidator(/password/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      }),
+      alternateEmails: this.fb.array([]) 
+    }, {validator: passwordValidator});
+
+    this.registrationForm.get('subscribe').valueChanges.subscribe(checkedValue => {
+      const email = this.registrationForm.get('email');
+      if(checkedValue) {
+        email.setValidators(Validators.required);
+      } else {
+        email.clearValidators();
+      }
+      email.updateValueAndValidity();
+    });
+  }
   
-  registrationForm = this.fb.group({
-    userName: ['sha'],
-    password: [''],
-    confirmPassword: [''],
-    address: this.fb.group({
-      city: [''],
-      state: [''],
-      postalCode: ['']
-    })
-
-  })
-
-  // registrationForm = new FormGroup({
-  //   userName: new FormControl('Sha'),
-  //   password: new FormControl(''),
-  //   confirmPassword: new FormControl(''),
-  //   address: new FormGroup({
-  //     city: new FormControl(''),
-  //     state: new FormControl(''),
-  //     postalCode: new FormControl(''),
-  //   })
-  // });
-
   public loadApiData() {
     this.registrationForm.patchValue({
       userName: 'SHA',
@@ -41,4 +65,11 @@ export class AppComponent {
     });
   }
 
+  public onSubmit(){
+    console.log(this.registrationForm.value);
+    this.reistrationService.register(this.registrationForm.value).subscribe(
+      response => console.log('Success', response),
+      error => console.log('Error', error)
+    );
+  }
 }
